29bfb50c09fd5da07e2fa9da042ebf804d65a665
Jake <jake@jakefixestech.com>
2026-04-12 16:32:40 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://pro2.jakefixestech.com/',
+  site: 'https://pro3.jakefixestech.com/',
   vite: {
     plugins: [tailwindcss()],
     build: {
